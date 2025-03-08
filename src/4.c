1475735db00798eb4a0e3d17e64ced949aa4abf9
nancee82o <nancee82o@gmx.com>
2025-03-08 20:38:25 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,11 @@
+  #include <stdio.h>
+  
+  int main() {
+      float x = 23.5;
+      float y = 14.7;
+      float z = (x + y) / 2;
+      
+      printf("The average of %f and %f is %f", x, y, z);
+      
+      return 0;
+  }
